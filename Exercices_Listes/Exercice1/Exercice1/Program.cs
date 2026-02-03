32532b91f029cf8e58e25e1b9337ee2c4b697d48
Maxim <maximrodriguez94@gmail.com>
2026-02-03 12:17:21 +0100

    Ajout Exercices 1 et 2 Listes

---
+++ Exercices_Listes/Exercice1/Exercice1/Program.cs
@@ -0,0 +1,24 @@
+﻿List<string> mots = new List<string>();
+string saisie = "";
+
+while (saisie != "stop")
+{
+    Console.WriteLine("Entrez un mot");
+    saisie = Console.ReadLine();
+
+    if (saisie != "stop")
+    {
+        mots.Add(saisie);
+    }
+}
+
+Console.WriteLine($"\n Nombre total de mots : {mots.Count}");
+
+Console.WriteLine("Liste des mots :");
+
+foreach(string mot in mots)
+{
+    Console.WriteLine(mot);
+}
+
+Console.ReadLine();
---
+++ Exercices_Listes/Exercice2/Exercice2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+List<int> temperature = new List<int>();
+
+int saisie = 0;
+int nbInvalides = 0; 
+
+while (saisie != -999)
+{
+    Console.WriteLine("Entrez une temperature");
+    saisie = int.Parse(Console.ReadLine());
+
+    if (saisie == -999)
+    {
+        break;
+    }
+
+    else if (saisie >= -50 && saisie <= 50)
+    {
+        temperature.Add(saisie);
+    }
+
+    else
+    {
+        nbInvalides++;
+    }
+}
+
+Console.WriteLine($"\n Nombre de temperatures valides : {temperature.Count}");
+
+Console.WriteLine($"\n Nombre de temperatures invalides : {nbInvalides}");
+
+int min = temperature.Min();
+int max = temperature.Max();
+
+Console.WriteLine($"Temperature mini : {min}");
+Console.WriteLine($"temperature max : {max}");
